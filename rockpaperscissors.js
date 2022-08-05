f0008349b4fa4ef8a9a8f748e886fd6eb9d95777
Octa717 <duiuoctavian@gmail.com>
2022-08-05 18:14:57 +0300

    Add logic without styling

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,45 +1,56 @@
-let playerSelection;
-        let computerSelection;
-
-        function getComputerChoice() {
+function getComputerChoice() {
         
-        computerSelection = (min,max) => (Math.floor(Math.random() * (max - min + 1) ) + min);
+let computerSelection = (min,max) => (Math.floor(Math.random() * (max - min + 1) ) + min);
 
-        return computerSelection(1,3);
+return computerSelection(1,3);
 
+}
+
+function playRound(playerSelection, computerSelection) {
+
+// 1 = Rock; 2 = Paper; 3 = Scissors.
+let result = document.createElement('div');
+
+
+if (playerSelection === computerSelection) result.textContent= 'Tie!';
+else if (playerSelection === 1 && computerSelection === 2) result.textContent = 'You Lose! Paper beats Rock!';
+else if (playerSelection === 1 && computerSelection === 3) result.textContent = 'You Win! Rock beats Scissors!';
+else if (playerSelection === 2 && computerSelection === 1) result.textContent = 'You Win! Paper beats Rock!';
+else if (playerSelection === 2 && computerSelection === 3) result.textContent = 'You Lose! Scissors beats Paper!';
+else if (playerSelection === 3 && computerSelection === 1) result.textContent = 'You Lose! Rock beats Scissors!';
+else if (playerSelection === 3 && computerSelection === 2) result.textContent = 'You Win! Scissors beats Paper!';
+
+document.body.appendChild(result);
+
+
+}
+
+function getPlayerChoice() {
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+
+    button.addEventListener('click', () => {
+
+        switch(button.innerText) {
+
+            case 'Rock':
+                playRound(1, getComputerChoice());
+                break;
+            case 'Paper':
+                playRound(2, getComputerChoice());
+                break;
+            case 'Scissors':
+                playRound(3, getComputerChoice());
+                break;
         }
 
-        function playRound(playerSelection, computerSelection) {
+    });
 
-            // 1 = Rock; 2 = Paper; 3 = Scissors.
-            console.log(playerSelection);
-            console.log(computerSelection);
-            if (playerSelection === computerSelection) console.log("Tie!");
-            else if (playerSelection === 1 && computerSelection === 2) console.log("You Lose! Paper beats Rock!");
-            else if (playerSelection === 1 && computerSelection === 3) console.log("You Win! Rock beats Scissors!");
-            else if (playerSelection === 2 && computerSelection === 1) console.log("You Win! Paper beats Rock!");
-            else if (playerSelection === 2 && computerSelection === 3) console.log("You Lose! Scissors beats Paper!");
-            else if (playerSelection === 3 && computerSelection === 1) console.log("You Lose! Rock beats Scissors!");
-            else if (playerSelection === 3 && computerSelection === 2) console.log("You Win! Scissors beats Paper!");
-            else console.log("Please enter Rock, Paper or Scissors");
+});
 
-        }
+}
 
-        function getPlayerChoice() {
+getPlayerChoice();
 
-            let answer = prompt("Rock, Paper or Scissors?").toUpperCase();
-
-            if (answer === "ROCK") return 1;
-            if (answer === "PAPER") return 2;
-            if (answer === "SCISSORS") return 3;
-
-            return 4;
-
-        }
-
-        function game() {
-
-            for (let i = 0; i < 5; i++) playRound(getPlayerChoice(), getComputerChoice());
-        }
-
-        game();
